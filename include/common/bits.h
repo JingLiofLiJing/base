44b6cccbaae937d134b481d1cb937d682fb40306
JingLiofLiJing <526353363@qq.com>
2022-07-01 22:49:52 -0400

    Adopted c++17 inline variables in namespace as possible.

--- include/common/bits.h
+++ include/common/bits.h
@@ -1,15 +1,27 @@
 #pragma once
 
+#include <cstdint>
+
 #include <cpu_features/cpu_features_macros.h>
+
+#ifdef CPU_FEATURES_ARCH_X86
+#  include <cpu_features/cpuinfo_x86.h>
+#endif  // CPU_FEATURES_ARCH_X86
+
+#include "common/namespace.h"
+
+BASE_NS_BEGIN
 
 #ifdef CPU_FEATURES_ARCH_X86
 
 #ifdef CPU_FEATURES_ARCH_X86_32
-#  define B_WORD_BITS 32
-#  define B_WORD_BYTES 4
+inline constexpr uint32_t word_bits = 32u;
+inline constexpr uint32_t word_bytes = 4u;
 #elif defined(CPU_FEATURES_ARCH_X86_64)
-#  define B_WORD_BITS 64
-#  define B_WORD_BYTES 8
+inline constexpr uint32_t word_bits = 64u;
+inline constexpr uint32_t word_bytes = 8u;
 #endif  // CPU_FEATURES_ARCH_X86_32
 
 #endif  // CPU_FEATURES_ARCH_X86
+
+BASE_NS_END
--- include/common/cpu_features.h
+++ include/common/cpu_features.h
@@ -21,42 +21,42 @@ get_cpu_info() {
 inline cpu_features::X86Features
 get_cpu_features() { return get_cpu_info()->features; }
 
-inline bool enable_popcnt() { return static_cast<bool>(get_cpu_features().popcnt); }
+inline const bool enable_popcnt = static_cast<bool>(get_cpu_features().popcnt);
 
 #if CPU_FEATURES_COMPILED_X86_SSE
-constexpr bool enable_sse() { return true; }
+inline constexpr bool enable_sse = true;
 #else
-inline bool enable_sse() { return static_cast<bool>(get_cpu_features().sse); }
+inline const bool enable_sse = static_cast<bool>(get_cpu_features().sse);
 #endif
 
 #if CPU_FEATURES_COMPILED_X86_SSE2
-constexpr bool enable_sse2() { return true; }
+inline constexpr bool enable_sse2 = true;
 #else
-inline bool enable_sse2() { return static_cast<bool>(get_cpu_features().sse2); }
+inline const bool enable_sse2 = static_cast<bool>(get_cpu_features().sse2);
 #endif
 
 #if CPU_FEATURES_COMPILED_X86_SSE4_1
-constexpr bool enable_sse4_1() { return true; }
+inline constexpr bool enable_sse4_1 = true;
 #else
-inline bool enable_sse4_1() { return static_cast<bool>(get_cpu_features().sse4_1); }
+inline const bool enable_sse4_1 = static_cast<bool>(get_cpu_features().sse4_1);
 #endif
 
 #if CPU_FEATURES_COMPILED_X86_SSE4_2
-constexpr bool enable_sse4_2() { return true; }
+inline constexpr bool enable_sse4_2 = true;
 #else
-inline bool enable_sse4_2() { return static_cast<bool>(get_cpu_features().sse4_2); }
+inline const bool enable_sse4_2 = static_cast<bool>(get_cpu_features().sse4_2);
 #endif
 
 #if CPU_FEATURES_COMPILED_X86_AVX
-constexpr bool enable_avx() { return true; }
+inline constexpr bool enable_avx = true;
 #else
-inline bool enable_avx() { return static_cast<bool>(get_cpu_features().avx); }
+inline const bool enable_avx = static_cast<bool>(get_cpu_features().avx);
 #endif
 
 #if CPU_FEATURES_COMPILED_X86_AVX2
-constexpr bool enable_avx2() { return true; }
+inline constexpr bool enable_avx2 = true;
 #else
-inline bool enable_avx2() { return static_cast<bool>(get_cpu_features().avx2); }
+inline const bool enable_avx2 = static_cast<bool>(get_cpu_features().avx2);
 #endif
 
 #endif  // CPU_FEATURES_ARCH_X86
