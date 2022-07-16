129307521bce38cf5f1cfa451807c371f3a1804c
JingLiofLiJing <526353363@qq.com>
2022-07-16 10:25:21 -0400

    Changed BASE_ADD_METHOD(S) macro in status.

--- include/status/status.h
+++ include/status/status.h
@@ -13,17 +13,17 @@ public:
     explicit status(rcode code) : details::tagged_state(code) {}
     ~status();
 
-#define BASE_ADD_METHOD(rc)                                    \
+#define BASE_ADD_METHODS(rc)                                   \
     static status rc() { return status(rcode::rc); }           \
     bool is_##rc() const {                                     \
         return is_rcode_only() && (only_rcode() == rcode::rc); \
     }
 
-    BASE_ADD_METHOD(ok)
-    BASE_ADD_METHOD(internal_error)
-    BASE_ADD_METHOD(unknown_error)
+    BASE_ADD_METHODS(ok)
+    BASE_ADD_METHODS(internal_error)
+    BASE_ADD_METHODS(unknown_error)
 
-#undef BASE_ADD_METHOD
+#undef BASE_ADD_METHODS
 };
 
 BASE_NS_END
